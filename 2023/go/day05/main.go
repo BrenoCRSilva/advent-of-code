7b0c552e7193f39e1a4bee0870a2fbdd9b3a538b
BrenoCRSilva <brenocrs@gmail.com>
2024-11-22 01:58:51 -0300

    implemented solution for part 1

--- 2023/go/day05/main.go
+++ 2023/go/day05/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	_ "embed"
+	"log"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -11,14 +13,32 @@ var input string
 
 func init() {
 	input = strings.TrimRight(input, "\n")
+	input = strings.TrimSpace(input)
 	if len(input) == 0 {
-		panic("empty input file")
+		log.Fatal("input.txt is empty")
 	}
 }
 
-func ParseInput(input string) ([]int, [][]Map) {
+func main() {
+	listOfSeeds, listOfMapBlocks := parseInput(input)
+	closest := getClosestLocation(listOfSeeds, listOfMapBlocks)
+	println(closest)
+}
+
+func getClosestLocation(listOfSeeds []int, listOfMapBlocks [][]Map) int {
+	closest := math.MaxInt
+	for _, seed := range listOfSeeds {
+		location := getMap(seed, listOfMapBlocks)
+		if location < closest {
+			closest = location
+		}
+	}
+	return closest
+}
+
+func parseInput(input string) ([]int, [][]Map) {
 	var listOfSeeds []int
-	var listOfMapRanges [][]Map
+	var listOfMapBlocks [][]Map
 	document := strings.Split(input, "\n\n")
 	seeds := strings.Split(document[0], " ")
 	for i := 1; i < len(seeds); i++ {
@@ -26,22 +46,35 @@ func ParseInput(input string) ([]int, [][]Map) {
 		listOfSeeds = append(listOfSeeds, seed)
 	}
 	for j := 1; j < len(document); j++ {
-		mapLines := strings.Split(document[j], "\n")[1:]
+		mapBlocks := strings.Split(document[j], "\n")[1:]
 		var ranges []Map
-		for _, line := range mapLines {
-			fields := strings.Split(line, " ")
-			source, _ := strconv.Atoi(fields[1])
-			destination, _ := strconv.Atoi(fields[0])
-			count, _ := strconv.Atoi(fields[2])
+		for _, block := range mapBlocks {
+			lines := strings.Split(block, " ")
+			source, _ := strconv.Atoi(lines[1])
+			destination, _ := strconv.Atoi(lines[0])
+			count, _ := strconv.Atoi(lines[2])
 			ranges = append(ranges, Map{
 				source:      source,
 				destination: destination,
 				count:       count,
 			})
-			listOfMapRanges = append(listOfMapRanges, ranges)
+		}
+		listOfMapBlocks = append(listOfMapBlocks, ranges)
+	}
+	return listOfSeeds, listOfMapBlocks
+}
+
+func getMap(seed int, listOfMapBlocks [][]Map) int {
+	locations := seed
+	for _, block := range listOfMapBlocks {
+		for _, b := range block {
+			if seed >= b.source && seed <= b.source+b.count {
+				locations = b.destination + (locations - b.source)
+				break
+			}
 		}
 	}
-	return listOfSeeds, listOfMapRanges
+	return locations
 }
 
 type Map struct {
