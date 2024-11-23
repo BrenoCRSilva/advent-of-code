53cfccfe94b28d5a1e4371fad2aa83180d48b0f6
BrenoCRSilva <brenocrs@gmail.com>
2024-11-23 01:13:14 -0300

    solution for part 1

--- 2023/go/day06/main.go
+++ 2023/go/day06/main.go
@@ -3,41 +3,62 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"log"
+	"strconv"
 	"strings"
 )
 
 //go:embed input.txt
 var input string
 
-type Race struct {
-	time, distance int
+func init() {
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		log.Fatal("input.txt is empty")
+	}
 }
 
 func main() {
-	line := parseInput(input)
-	fmt.Println(line)
+	time, distance := parseInput(input)
+	fmt.Println(time)
+	fmt.Println(distance)
+	fmt.Println(getWinningRaces(time, distance))
 }
 
-func parseInput(input string) [][]string {
+func parseInput(input string) (time []int, distance []int) {
 	lines := strings.Split(input, "\n")
-	var listOfLines [][]string
-	for _, line := range lines {
-		a := strings.Split(line, " ")
-		listOfLines = append(listOfLines, a[1:])
+	for i, line := range lines {
+		a := strings.Split(line, ":")
+		data := strings.Trim(a[1], " ")
+		vars := strings.Split(data, " ")
+		for _, v := range vars {
+			v = strings.Trim(v, " ")
+			number, err := strconv.Atoi(v)
+			if err != nil {
+				continue
+			}
+			if i == 0 {
+				time = append(time, number)
+			} else if i == 1 {
+				distance = append(distance, number)
+			}
+		}
 	}
-	return listOfLines
+	return time, distance
 }
 
-func getWinningRaces(races []Race) int {
-	var count int
-	var speed int
-	for _, race := range races {
-		for speed < race.time {
-			if (race.time-speed)*speed > race.distance {
+func getWinningRaces(time []int, distance []int) int {
+	wins := 1
+	for i := 0; i < len(time); i++ {
+		var speed int
+		var count int
+		for speed < time[i] {
+			if (time[i]-speed)*speed > distance[i] {
 				count++
 			}
 			speed++
 		}
+		wins *= count
 	}
-	return count
+	return wins
 }
